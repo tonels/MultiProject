a90e879ac4cd0341e8be2b6eced739b958719baa
liangshuai <1589900136@qq.com>
2019-12-01 16:56:17 +0800

    代码调整

--- queryDsl/DslDemo1/src/main/java/querydsl/repo/THotelRepo.java
+++ queryDsl/DslDemo1/src/main/java/querydsl/repo/THotelRepo.java
@@ -1,14 +1,11 @@
 package querydsl.repo;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import querydsl.entity.THotel;
-import querydsl.repo.customer.HotelRepoCust;
 import querydsl.repo.customer.HotelRepoCustom2;
 
 @Repository
-public interface THotelRepo extends BaseRepository<THotel> , HotelRepoCustom2 {
+public interface THotelRepo extends BaseRepository<THotel>, HotelRepoCustom2 {
 
 }
 
