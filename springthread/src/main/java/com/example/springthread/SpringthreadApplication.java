f42a117b7e003bf21538d2f1b92ed1dfbeeeb5af
tonels <liangshuai0012018@outlook.com>
2020-05-23 02:45:35 +0800

    springthread

---
+++ springthread/src/main/java/com/example/springthread/SpringthreadApplication.java
@@ -0,0 +1,13 @@
+package com.example.springthread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringthreadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringthreadApplication.class, args);
+    }
+
+}
---
+++ springthread/src/test/java/com/example/springthread/SpringthreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springthread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringthreadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
