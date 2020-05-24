3a776071c8002a1b354a25b63f78f0013f98cfe3
tonels <liangshuai0012018@outlook.com>
2020-05-25 01:16:27 +0800

    java8 Stream

---
+++ java8/src/main/java/samples/stream/peek/Te.java
@@ -0,0 +1,170 @@
+package samples.stream.peek;
+
+import com.google.common.collect.Lists;
+import org.junit.Before;
+import org.junit.Test;
+import samples.lambda.Person2;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Te {
+
+    private List<Person2> list;
+    private List<String> strings;
+
+    // 初始化数据需要的
+    @Before
+    public void inti() {
+        Person2 p1 = new Person2(13, "男", "张2");
+        Person2 p2 = new Person2(2, "男", "张6");
+        Person2 p3 = new Person2(4, "男", "张1");
+        Person2 p4 = new Person2(6, "女", "张3");
+        Person2 p5 = new Person2(1, "男", "张5");
+        Person2 p6 = new Person2(10, "男", "张8");
+        Person2 p7 = new Person2(3, "女", "张4");
+        Person2 p8 = new Person2(31, "女", "张4");
+        Person2 p9 = new Person2(21, "女", "张4");
+        Person2 p10 = new Person2(19, "女", "张4");
+
+        list = Lists.newArrayList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10);
+        strings = Lists.newArrayList("a1", "a2", "b1", "c2", "c1");
+    }
+
+    @Test
+    public void t1(){
+//        list.stream().peek()
+
+
+    }
+
+    /**
+     * will filter 1
+     * will filter 10
+     * 10
+     */
+    @Test
+    public void t2(){
+        List<Integer> list = Arrays.asList(1, 10, 3, 7, 5);
+        int a = list.stream()
+                .peek(num -> System.out.println("will filter " + num))
+                .filter(x -> x > 5)
+                .findFirst()
+                .get();
+        System.out.println(a);
+    }
+
+    /**
+     * will filter 1
+     * 1
+     */
+    @Test
+    public void t21(){
+        List<Integer> list = Arrays.asList(1, 10, 3, 7, 5);
+        int a = list.stream()
+                .peek(num -> System.out.println("will filter " + num))
+//                .filter(x -> x > 5)
+                .findFirst()
+                .get();
+        System.out.println(a);
+    }
+
+    /**
+     * will filter 1
+     * 1
+     */
+    @Test
+    public void t22(){
+        List<Integer> list = Arrays.asList(1, 10, 3, 7, 5);
+        final List<Integer> collect = list.stream()
+                .peek(num -> System.out.println("will filter " + num))
+                .filter(x -> x > 5)
+//                .findFirst()
+                .collect(Collectors.toList());
+        System.out.println(collect.size());
+    }
+
+    /**
+     * will filter 1
+     * will filter 10
+     * will filter 3
+     * will filter 7
+     * will filter 5
+     * 2
+     */
+    @Test
+    public  void testPeek() {
+        System.out.println();
+        System.out.println("Test peek start");
+        Collection<String> collection = Arrays.asList("a1", "a2", "a3", "a1");
+        // Print debug information for every element
+        System.out.print("peak1 = ");
+        List<String> peek = collection.stream().map(String::toUpperCase).peek((e) -> System.out.print(e + ",")).
+                collect(Collectors.toList());
+        System.out.println(); // print  peak1 = A1,A2,A3,A1,
+        System.out.println("peek2 = " + peek); // print  peek2 = [A1, A2, A3, A1]
+
+        Collection<StringBuilder> list = Arrays.asList(new StringBuilder("a1"), new StringBuilder("a2"), new StringBuilder("a3"));
+        List<StringBuilder> newList = list.stream().peek((p) -> p.append("_new")).collect(Collectors.toList());
+        System.out.println("newList = " + newList); // print  newList = [a1_new, a2_new, a3_new]
+    }
+
+    // spring 中操作
+
+//    private void updateFilters() {
+//
+//        if (this.filters.isEmpty()) {
+//            return;
+//        }
+//
+//        List<WebFilter> filtersToUse = this.filters.stream()
+//                .peek(filter -> {
+//                    if (filter instanceof ForwardedHeaderTransformer && this.forwardedHeaderTransformer == null) {
+//                        this.forwardedHeaderTransformer = (ForwardedHeaderTransformer) filter;
+//                    }
+//                })
+//                .filter(filter -> !(filter instanceof ForwardedHeaderTransformer))
+//                .collect(Collectors.toList());
+//
+//        this.filters.clear();
+//        this.filters.addAll(filtersToUse);
+//    }
+//
+
+
+//    @Override
+//    public Strategy createStrategyIfMatch(Class target, Method method) {
+//        if (switcher.isEmpty()) {
+//            return null;
+//        }
+//        String text = target.getName().concat(".").concat(method.getName());
+//
+//        return switcher.entrySet().stream()
+//                .filter(entry -> antPathMatcher.match(entry.getValue().getExpression(), text))
+//                .peek(entry -> entry.getValue().setId(entry.getKey()))
+//                .map(Map.Entry::getValue)
+//                .findFirst()
+//                .orElse(null);
+//    }
+
+    /**
+     * [1, 2, 3, 4]
+     * [2, 4, 6, 8]
+     */
+    @Test
+    public void t3(){
+        List<Integer> list = new ArrayList<>();
+        List<Integer> result = Stream.of(1, 2, 3, 4)
+                .peek(x -> list.add(x))
+                .map(x -> x * 2)
+                .collect(Collectors.toList());
+
+        System.out.println(list);
+        System.out.println(result);
+
+    }
+
+
+
+}
---
+++ streams-master/streams-master/src/test/java/part2/exercise/CollectorsExercise2.java
@@ -0,0 +1,257 @@
+package part2.exercise;
+
+import data.Employee;
+import data.JobHistoryEntry;
+import data.Person;
+import org.junit.Test;
+
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class CollectorsExercise2 {
+
+    private static String generateString() {
+        final String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        final int maxLength = 10;
+        final int length = ThreadLocalRandom.current().nextInt(maxLength) + 1;
+
+        return IntStream.range(0, length)
+                .mapToObj(letters::charAt)
+                .map(Object::toString)
+                .collect(Collectors.joining());
+    }
+
+    private static String[] generateStringArray(int length) {
+        return Stream.generate(CollectorsExercise2::generateString)
+                .limit(length)
+                .toArray(String[]::new);
+    }
+
+    public static String pickString(String[] array) {
+        return array[ThreadLocalRandom.current().nextInt(array.length)];
+    }
+
+    public static class Key {
+        private final String id;
+
+        public Key(String id) {
+            this.id = id;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Key key = (Key) o;
+
+            return id.equals(key.id);
+
+        }
+
+        @Override
+        public int hashCode() {
+            return id.hashCode();
+        }
+    }
+
+    public static class Value {
+        private final String keyId;
+
+        public Value(String keyId) {
+            this.keyId = keyId;
+        }
+
+        public String getKeyId() {
+            return keyId;
+        }
+    }
+
+    public static class Pair {
+        private final Key key;
+        private final Value value;
+
+        public Pair(Key key, Value value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public Key getKey() {
+            return key;
+        }
+
+        public Value getValue() {
+            return value;
+        }
+    }
+
+    public static List<Pair> generatePairs(int idCount, int length) {
+        final String[] ids = generateStringArray(idCount);
+
+        return Stream.generate(() -> new Pair(new Key(pickString(ids)), new Value(pickString(ids))))
+                .limit(length)
+                .collect(toList());
+    }
+
+    private static class SubResult {
+        private final Map<Key, List<Value>> subResult;
+        private final Map<String, List<Key>> knownKeys;
+        private final Map<String, List<Value>> valuesWithoutKeys;
+
+        public SubResult(Map<Key, List<Value>> subResult, Map<String, List<Key>> knownKeys, Map<String, List<Value>> valuesWithoutKeys) {
+            this.subResult = subResult;
+            this.knownKeys = knownKeys;
+            this.valuesWithoutKeys = valuesWithoutKeys;
+        }
+
+        public Map<Key, List<Value>> getSubResult() {
+            return subResult;
+        }
+
+        public Map<String, List<Value>> getValuesWithoutKeys() {
+            return valuesWithoutKeys;
+        }
+
+        public Map<String, List<Key>> getKnownKeys() {
+            return knownKeys;
+        }
+    }
+
+    private static class MapPair {
+        private final Map<String, Key> keyById;
+        private final Map<String, List<Value>> valueById;
+
+        public MapPair() {
+            this(new HashMap<>(), new HashMap<>());
+        }
+
+        public MapPair(Map<String, Key> keyById, Map<String, List<Value>> valueById) {
+            this.keyById = keyById;
+            this.valueById = valueById;
+        }
+
+        public Map<String, Key> getKeyById() {
+            return keyById;
+        }
+
+        public Map<String, List<Value>> getValueById() {
+            return valueById;
+        }
+    }
+
+    private static <K, V, M extends Map<K, V>>
+    BinaryOperator<M> mapMerger(BinaryOperator<V> mergeFunction) {
+        return (m1, m2) -> {
+            for (Map.Entry<K, V> e : m2.entrySet()) {
+                m1.merge(e.getKey(), e.getValue(), mergeFunction);
+            }
+            return m1;
+        };
+    }
+
+    @Test
+    public void collectKeyValueMap() {
+        final List<Pair> pairs = generatePairs(10, 100);
+
+        // В два прохода
+        // final Map<String, Key> keyMap1 = pairs.stream()...
+
+        // final Map<String, List<Value>> valuesMap1 = pairs.stream()...
+
+        // В каждом Map.Entry id ключа должно совпадать с keyId для каждого значения в списке
+        // final Map<Key, List<Value>> keyValuesMap1 = valueMap1.entrySet().stream()...
+
+        // В 1 проход в 2 Map с использованием MapPair и mapMerger
+        final MapPair res2 = pairs.stream()
+                .collect(new Collector<Pair, MapPair, MapPair>() {
+                    @Override
+                    public Supplier<MapPair> supplier() {
+                        // TODO
+                        throw new UnsupportedOperationException();
+                    }
+
+                    @Override
+                    public BiConsumer<MapPair, Pair> accumulator() {
+                        // TODO add key and value to maps
+                        throw new UnsupportedOperationException();
+                    }
+
+                    @Override
+                    public BinaryOperator<MapPair> combiner() {
+                        // TODO use mapMerger
+                        throw new UnsupportedOperationException();
+                    }
+
+                    @Override
+                    public Function<MapPair, MapPair> finisher() {
+                        return Function.identity();
+                    }
+
+                    @Override
+                    public Set<Characteristics> characteristics() {
+                        return Collections.unmodifiableSet(EnumSet.of(
+                                Characteristics.UNORDERED,
+                                Characteristics.IDENTITY_FINISH));
+                    }
+                });
+
+        final Map<String, Key> keyMap2 = res2.getKeyById();
+        final Map<String, List<Value>> valuesMap2 = res2.getValueById();
+
+        // final Map<Key, List<Value>> keyValuesMap2 = valueMap2.entrySet().stream()...
+
+        // Получение результата сразу:
+
+        final SubResult res3 = pairs.stream()
+                .collect(new Collector<Pair, SubResult, SubResult>() {
+                    @Override
+                    public Supplier<SubResult> supplier() {
+                        // TODO
+                        throw new UnsupportedOperationException();
+                    }
+
+                    @Override
+                    public BiConsumer<SubResult, Pair> accumulator() {
+                        // TODO add key to map, then check value.keyId and add it to one of maps
+                        throw new UnsupportedOperationException();
+                    }
+
+                    @Override
+                    public BinaryOperator<SubResult> combiner() {
+                        // TODO use mapMerger, then check all valuesWithoutKeys
+                        throw new UnsupportedOperationException();
+                    }
+
+                    @Override
+                    public Function<SubResult, SubResult> finisher() {
+                        // TODO use mapMerger, then check all valuesWithoutKeys
+                        throw new UnsupportedOperationException();
+                    }
+
+                    @Override
+                    public Set<Characteristics> characteristics() {
+                        return Collections.unmodifiableSet(EnumSet.of(
+                                Characteristics.UNORDERED));
+                    }
+                });
+
+        final Map<Key, List<Value>> keyValuesMap3 = res3.getSubResult();
+
+        // compare results
+    }
+
+}
