0b8cb049c7659ff4fc8c186b22cb61103a549c8f
liangshuai <1589900136@qq.com>
2020-06-02 14:51:05 +0800

    ss

--- commonTypeData/src/main/java/data/map/MapTest.java
+++ commonTypeData/src/main/java/data/map/MapTest.java
@@ -1,16 +1,12 @@
 package data.map;
 
-import com.google.common.annotations.VisibleForTesting;
 import org.junit.Test;
-
-import java.util.Map;
 
 public class MapTest {
 
     @Test
-    public void t1(){
+    public void t1() {
 //        Map
-
 
 
     }
